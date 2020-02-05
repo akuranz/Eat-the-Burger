8a240a4b72691e48f1ff55a50c7905ede5f6a516
Abby Kuranz <akuranz@gmail.com>
2020-02-04 18:00:43 -0700

    first get route working with index handlebards file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,14 @@ var express = require("express");
 
 var router = express.Router();
 
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var burgerObject = {
+      burgers: data
+    };
+    console.log(burgerObject);
+    res.render("index", burgerObject);
+  });
+});
+
 module.exports = router;
