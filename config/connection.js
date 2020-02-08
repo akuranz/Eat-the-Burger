3d907a201ffff39fa9af251875e2bcc31cd743a4
Abby Kuranz <akuranz@gmail.com>
2020-02-07 18:52:25 -0700

    added JAWSDB addon for heroku push

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,18 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Make connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+}
 
 // Export connection for our ORM to use.
 module.exports = connection;
